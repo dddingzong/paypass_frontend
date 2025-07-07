b701f1df7564be859a7439589b99a05f38221d56
USER <cji513@naver.com>
2025-07-07 13:53:41 +0900

    chore(App): MapPage 분리로 인한 변경

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import LinkPage from '@/app/LinkPage';
 import LogPage from '@/app/LogPage';
-import MapPage from '@/app/MapPage';
+import MapRouterPage from '@/app/MapRouterPage';
 import MyPage from '@/app/MyPage';
 import SelectRole from '@/app/SelectRole';
+import index from '@/app/index'; // 경로는 실제 파일 위치에 맞게 조정하세요
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
@@ -13,8 +14,9 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="SelectRole" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" component={index} />
         <Stack.Screen name="SelectRole" component={SelectRole} />
-        <Stack.Screen name="MapPage" component={MapPage} />
+        <Stack.Screen name="MapRouterPage" component={MapRouterPage} />
         <Stack.Screen name="LogPage" component={LogPage} />
         <Stack.Screen name="MyPage" component={MyPage} />
         <Stack.Screen name="LinkPage" component={LinkPage} />
