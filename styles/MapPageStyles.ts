4fc481c4221753a1d07cc13cdbbeaf820074bc79
USER <cji513@naver.com>
2025-07-23 18:16:10 +0900

    chore: 지도 api 설정 변경

--- styles/MapPageStyles.ts
+++ styles/MapPageStyles.ts
@@ -5,6 +5,11 @@ export const customMapStyle = [
     "stylers": [{ "visibility": "off" }]
   },
   {
+    "featureType": "poi.business",
+    "elementType": "labels",
+    "stylers": [{ "visibility": "off" }]
+  },
+  {
     "featureType": "transit.station.bus",
     "elementType": "labels",
     "stylers": [{ "visibility": "off" }]
